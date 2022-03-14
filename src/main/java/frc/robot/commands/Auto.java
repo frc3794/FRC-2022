da7dbf38097b1911a45c35807ac5bf2c14be4af1
Manuel Díaz <ceo@macnolo.net>
2022-03-14 12:37:27 -0600

    Creating Autonomous (2 pts)

---
+++ src/main/java/frc/robot/commands/Auto.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+public class Auto extends CommandBase {
+
+  private final Drivetrain m_drivetrain;
+  private final Timer timer = new Timer();
+
+  /** Creates a new Auto. */
+  public Auto(Drivetrain drivetrain) {
+    this.m_drivetrain = drivetrain;
+    addRequirements(m_drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    timer.reset();
+    timer.start();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    do {
+      this.m_drivetrain.arcadeDrive(-0.6, 0);
+    } while (timer.get() < 2);
+
+    this.m_drivetrain.stop();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+}
